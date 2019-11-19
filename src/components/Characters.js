8920406ae02d44b367467f8c69b0cfb805f42e2d
sanvean74 <dlambertpdx@gmail.com>
2019-11-18 22:13:09 -0800

    Characters component created

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,1 +1,27 @@
+import React from 'react';
+import Character from './Character';
+import PropTypes from 'prop-types';
 
+const Characters = ({ characters }) => {
+  const charElements = characters.map((character, i) => {
+    <li key={character._id || i}>
+      <Character {...character} />
+    </li>;
+  });
+
+  return (
+    <ul>
+      {charElements}
+    </ul>
+  );
+};
+
+Characters.propTypes = {
+  characters: PropTypes.arrayOf(PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    photoUrl: PropTypes.string.isRequired
+  })).isRequired
+};
+
+export default Characters;

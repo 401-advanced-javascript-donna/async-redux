f13b8c79bf5c5f160ce16d4ef7079123bc8a8f43
sanvean74 <dlambertpdx@gmail.com>
2019-11-18 21:35:19 -0800

    reducer created

---
+++ src/reducers/characterReducer.js
@@ -0,0 +1,18 @@
+import { FETCH_CHARACTERS_LOADING, FETCH_CHARACTERS_DONE, FETCH_CHARACTERS } from '../actions/charactersAction';
+
+const initialState = {
+  charList: [],
+  charLoading: false
+};
+
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+    case FETCH_CHARACTERS_LOADING:
+    case FETCH_CHARACTERS_DONE:
+      return { ...state, charLoading: !state.charLoading };
+    case FETCH_CHARACTERS:
+      return { ...state, charLIst: action.payload };
+    default:
+      return state;
+  }
+}

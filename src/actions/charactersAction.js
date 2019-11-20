15a385db48f25d98ae9da6cba8c398a4c5ba2578
sanvean74 <dlambertpdx@gmail.com>
2019-11-20 10:06:36 -0800

    using promises middleware

--- src/actions/charactersAction.js
+++ src/actions/charactersAction.js
@@ -4,20 +4,28 @@ export const FETCH_CHARACTERS = 'FETCH_CHARACTERS';
 export const FETCH_CHARACTERS_LOADING = 'FETCH_CHARACTERS_LOADING';
 export const FETCH_CHARACTERS_DONE = 'FETCH_CHARACTERS_DONE';
 
-export const updateCharacters = () => dispatch => {
-  dispatch({
-    type: FETCH_CHARACTERS_LOADING
-  });
+// Promises Middleware
+export const updateCharacters = () => ({
+  type: FETCH_CHARACTERS,
+  pendingType: FETCH_CHARACTERS_LOADING,
+  fulfilledType: FETCH_CHARACTERS_DONE,
+  payload: fetchCharacters()
+});
 
-  return fetchCharacters()
-    .then(characters => {
-      dispatch({
-        type: FETCH_CHARACTERS,
-        payload: characters
-      });
-      dispatch({
-        type: FETCH_CHARACTERS_DONE
-      });
-    });
+// export const updateCharacters = () => dispatch => {
+//   dispatch({
+//     type: FETCH_CHARACTERS_LOADING
+//   });
 
-};
+//   return fetchCharacters()
+//     .then(characters => {
+//       dispatch({
+//         type: FETCH_CHARACTERS,
+//         payload: characters
+//       });
+//       dispatch({
+//         type: FETCH_CHARACTERS_DONE
+//       });
+//     });
+
+// };
